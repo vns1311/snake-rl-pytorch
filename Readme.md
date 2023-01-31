0ba74c881c2debde025a3c4a7b2b554185b0c52f
Shyam Kumar V N <vn.shyamkumar@gmail.com>
2023-01-31 21:43:28 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Packages Required
+1. pygame
+2. pytorch (without CUDA)
+3. 
---
+++ model.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+class Linear_QNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model ,lr, gamma) -> None:
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+    
+    def train_step(self, current_state, action, reward, next_state, game_over):
+        current_state = torch.tensor(current_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+
+        if len(current_state.shape) == 1:
+            current_state = torch.unsqueeze(current_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            game_over = (game_over, )
+        
+        # 1: Get the Predicted Value with current state
+        prediction = self.model(current_state)
+        # 2: Get the new iteration
+        target = prediction.clone()
+        for idx in range(len(game_over)):
+            q_new = reward[idx]
+            if not game_over[idx]:
+                q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+            target[idx][torch.argmax(action[idx]).item()] = q_new
+        
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, prediction)
+        loss.backward()
+        self.optimizer.step()
